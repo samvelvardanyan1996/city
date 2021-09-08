23bf22746633e21b7afb065be56a7b53b5bac16c
Samvel Vardanyan <developer.vardanyan@gmail.com>
2021-09-08 18:00:44 +0400

    deleted src1 folder

--- src1/components/Content/Posts/Post/MainOfThePostContent/PostInformationPartTwo/PostScore/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const PostScore = ({score}) => {
-  return (
-    <div className="postScore">{score}%</div>
-  );
-}
-
-export default PostScore;
--- src1/components/Header/User/Avatar/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Avatar = () => {
-  return (
-    <img className="img avatar" src="../src/images/avatar/1.jpg" />
-  );
-}
-
-export default Avatar;
